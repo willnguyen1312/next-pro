9f7dcb8951d5d7f1fbb45f62233ebf23d649bb74
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-03 17:00:12 -0300

    🎢

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { buttonVariants } from "@/components/ui/button"
+import { DatePickerWithRange } from "@/components/DatePickerWithRange"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+        <Avatar>
+          <AvatarImage
+            src="https://github.com/willnguyen1312.png"
+            alt="@nam.nguyen.node"
+          />
+          <AvatarFallback>NN</AvatarFallback>
+        </Avatar>
+
+        <DatePickerWithRange />
       </div>
       <div className="flex gap-4">
         <Link
